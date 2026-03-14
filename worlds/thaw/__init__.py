37f75ab99fb9b5d1d2774a1d50af1956982b1803
TheLivingShadow97 <hawestarrak@gmail.com>
2026-03-13 22:39:24 -0700

    we're generating, still only hollywood

--- worlds/thaw/__init__.py
+++ worlds/thaw/__init__.py
@@ -8,4 +8,5 @@
 
 # The main thing we do in our __init__.py is importing our world class from our world.py to initialize it.
 # Obviously, this world class needs to exist first. For this, read world.py.
+# Generation works les go!
 from .world import THAWWorld as THAWWorld
